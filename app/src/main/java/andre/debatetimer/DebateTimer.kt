54d24942dc5cb57e42d3dd317eb62b4d0b9d87f7
Andre <andrehsu6@gmail.com>
2017-05-09 00:51:12 +0800

    start newInstance of Timer on pause and resume

--- app/src/main/java/andre/debatetimer/DebateTimer.kt
+++ app/src/main/java/andre/debatetimer/DebateTimer.kt
@@ -7,53 +7,47 @@ import andre.debatetimer.extensions.abs
  * Created by Andre on 5/4/2017.
  */
 abstract class DebateTimer(timerOption: TimerOption) {
-	private val timer: Timer
-	
-	init {
-		timer = object : Timer(1000) {
-			override fun onTick() {
-				if (!paused) {
-					countUpSeconds++
-					countDownSeconds--
-					
-					val absVal = countDownSeconds.abs()
-					secondsUntilEnd = absVal % 60
-					minutesUntilEnd = absVal / 60
-					
-					secondsSinceStart = countUpSeconds % 60
-					minutesSinceStart = countUpSeconds / 60
-					
-					bellsSinceStart[countUpSeconds]?.ring()
-					
-					if (countDownSeconds <= 0 && countDownSeconds % 15 == 0) {
-						DebateBell.TWICE.ring()
-						if (countDownSeconds <= -120) {
-							stop()
-						}
-					}
-					
-					isOvertime = countDownSeconds < 0
-					
-					onSecond()
-				}
-			}
-		}.start()
-	}
-	
 	private var countUpSeconds: Int = -1
 	private var countDownSeconds: Int = timerOption.seconds + 1
-	private var paused = true
+	private var timer: Timer = newTimerInstance()
+	
+	fun newTimerInstance() = object : Timer(1000) {
+		override fun onTick() {
+			countUpSeconds++
+			countDownSeconds--
+			
+			val absVal = countDownSeconds.abs()
+			secondsUntilEnd = absVal % 60
+			minutesUntilEnd = absVal / 60
+			
+			secondsSinceStart = countUpSeconds % 60
+			minutesSinceStart = countUpSeconds / 60
+			
+			bellsSinceStart[countUpSeconds]?.ring()
+			
+			if (countDownSeconds <= 0 && countDownSeconds % 15 == 0) {
+				DebateBell.TWICE.ring()
+				if (countDownSeconds <= -120) {
+					stop()
+				}
+			}
+			
+			isOvertime = countDownSeconds < 0
+			
+			onSecond()
+		}
+	}
 	
 	fun pause() {
-		paused = true
+		timer.cancel()
+		timer = newTimerInstance()
 	}
 	
 	fun resume() {
-		paused = false
+		timer.start()
 	}
 	
 	fun stop() {
-		pause()
 		timer.cancel()
 	}
 	
