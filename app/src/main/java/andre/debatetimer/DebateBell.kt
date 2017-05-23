dd73472c8b68db9cf4631b588781140fd1260962
Andre <andrehsu6@gmail.com>
2017-05-23 17:56:09 +0800

    capitalize remaining and elapsed refactor DebateBell

---
+++ app/src/main/java/andre/debatetimer/DebateBell.kt
@@ -0,0 +1,18 @@
+package andre.debatetimer
+
+import andre.debatetimer.extensions.EnvVars
+import android.media.MediaPlayer
+
+enum class DebateBell(private val bell: MediaPlayer) {
+	ONCE(MediaPlayer.create(EnvVars.applicationContext, R.raw.debate_bell_one)),
+	TWICE(MediaPlayer.create(EnvVars.applicationContext, R.raw.debate_bell_two));
+	
+	companion object {
+		var debateBellEnabled = true
+	}
+	
+	@Synchronized fun ring() {
+		if (debateBellEnabled)
+			bell.start()
+	}
+}
