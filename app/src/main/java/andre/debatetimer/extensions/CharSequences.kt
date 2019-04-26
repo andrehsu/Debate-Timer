88eaf9fbce16e628b010999762c2720342b9f901
Andre <andrehsu6@gmail.com>
2019-04-27 01:10:52 +0900

    remove unused extension functions update dependencies change target SDK level to Q

--- app/src/main/java/andre/debatetimer/extensions/CharSequences.kt
+++
@@ -1,5 +0,0 @@
-package andre.debatetimer.extensions
-
-operator fun CharSequence.invoke(start: Int = 0, end: Int = length): String = substring(start, if (end >= 0) end else end + length)
-
-operator fun String.invoke(start: Int = 0, end: Int = length, step: Int): String = slice(start..(if (end >= 0) end else end + length) step step)
