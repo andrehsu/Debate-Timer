ac0aa8ef4b0288ff921d95fffa7f4940e4003fa4
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-08-27 11:18:51 +0800

    Change applicationId Fix back button dialog Only show back button dialog when timer is running

--- app/src/main/java/org/debatetimer/ui/main/MainFragment.kt
+++ app/src/main/java/org/debatetimer/ui/main/MainFragment.kt
@@ -1,15 +1,20 @@
 package org.debatetimer.ui.main
 
 import android.animation.LayoutTransition
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.snackbar.Snackbar
 import org.debatetimer.AppResources
+import org.debatetimer.R
 import org.debatetimer.databinding.MainFragmentBinding
 import org.debatetimer.databinding.TimerButtonBinding
 
@@ -18,31 +23,33 @@ class MainFragment : Fragment() {
     private val model: MainModel by viewModels()
     private lateinit var binding: MainFragmentBinding
     private lateinit var res: AppResources
+    private lateinit var callback: OnBackPressedCallback
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        callback = object : OnBackPressedCallback(false) {
+            override fun handleOnBackPressed() {
+                class ExitDialogFragment : DialogFragment() {
+                    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+                        return AlertDialog.Builder(requireContext())
+                                .setTitle(R.string.exit_question)
+                                .setPositiveButton(android.R.string.yes) { _, _ -> requireActivity().finish() }
+                                .setNegativeButton(android.R.string.no) { _, _ -> dialog!!.cancel() }
+                                .create()
+                    }
+                }
+                
+                ExitDialogFragment().show(parentFragmentManager, null)
+            }
+        }
+        
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
     
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-//        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true /* enabled by default */) {
-//            override fun handleOnBackPressed() {  class ExitDialogFragment : DialogFragment() {
-//                override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//                    return AlertDialog.Builder(requireContext())
-//                            .setTitle(R.string.exit_question)
-//                            .setPositiveButton(android.R.string.yes) { _, _ -> this@MainActivity.finish() }
-//                            .setNegativeButton(android.R.string.no) { _, _ -> dialog!!.cancel() }
-//                            .create()
-//                }
-//            }
-//
-//                if (isTaskRoot) {
-//                    ExitDialogFragment().show(supportFragmentManager, null)
-//                } else {
-//                    super.onBackPressed()
-//                }
-//            }
-//        }
-//        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-        
         binding = MainFragmentBinding.inflate(layoutInflater)
         res = AppResources.getInstance(requireContext())
         
@@ -53,8 +60,10 @@ class MainFragment : Fragment() {
         
         model.keepScreenOn.observe(viewLifecycleOwner, {
             if (it) {
+                callback.isEnabled = true
                 activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
             } else {
+                callback.isEnabled = false
                 activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
             }
         })
