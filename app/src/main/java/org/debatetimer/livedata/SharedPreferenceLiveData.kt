5c4b54f0a47ad1b7a82e57d91fa9bf0aca3dd403
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-08-28 17:24:49 +0800

    Improve SharedPreferenceLiveData.kt

--- app/src/main/java/org/debatetimer/livedata/SharedPreferenceLiveData.kt
+++ app/src/main/java/org/debatetimer/livedata/SharedPreferenceLiveData.kt
@@ -4,58 +4,52 @@ import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 
 fun SharedPreferences.getBooleanLiveData(key: String, default: Boolean): SharedPreferenceLiveData<Boolean> {
-    return object : SharedPreferenceLiveData<Boolean>(this, key, default) {
-        override fun getFromSp(sp: SharedPreferences, key: String, default: Boolean): Boolean {
-            return sp.getBoolean(key, default)
-        }
-        
-        override fun putToSp(editor: SharedPreferences.Editor, key: String, value: Boolean) {
-            editor.putBoolean(key, value)
-        }
-    }
+    return SharedPreferenceLiveData(
+            this,
+            key,
+            default,
+            { getBoolean(key, default) },
+            { putBoolean(key, it) }
+    )
 }
 
 fun SharedPreferences.getStringLiveData(key: String, default: String): SharedPreferenceLiveData<String> {
-    return object : SharedPreferenceLiveData<String>(this, key, default) {
-        override fun getFromSp(sp: SharedPreferences, key: String, default: String): String {
-            return sp.getString(key, default)!!
-        }
-        
-        override fun putToSp(editor: SharedPreferences.Editor, key: String, value: String) {
-            editor.putString(key, value)
-        }
-    }
+    return SharedPreferenceLiveData(
+            this,
+            key,
+            default,
+            { getString(key, default)!! },
+            { putString(key, it) }
+    )
 }
 
 fun <T> SharedPreferences.getObjectLiveData(key: String, defaultObjectAsString: String, objectFromString: (String) -> T, objectToString: (T) -> String): SharedPreferenceLiveData<T> {
-    return object : SharedPreferenceLiveData<T>(this, key, objectFromString(defaultObjectAsString)) {
-        override fun getFromSp(sp: SharedPreferences, key: String, default: T): T {
-            return objectFromString(sp.getString(key, defaultObjectAsString)!!)
-        }
-        
-        override fun putToSp(editor: SharedPreferences.Editor, key: String, value: T) {
-            editor.putString(key, objectToString(value))
-        }
-    }
+    return SharedPreferenceLiveData(
+            this,
+            key,
+            objectFromString(defaultObjectAsString),
+            { objectFromString(getString(key, defaultObjectAsString)!!) },
+            { putString(key, objectToString(it)) }
+    )
 }
 
-abstract class SharedPreferenceLiveData<T>(
+class SharedPreferenceLiveData<T>(
         private val sp: SharedPreferences,
         private val key: String,
-        private val default: T
+        private val default: T,
+        private val get: SharedPreferences.() -> T,
+        private val put: SharedPreferences.Editor.(value: T) -> Unit
 ) : LiveData<T>(), SharedPreferences.OnSharedPreferenceChangeListener {
     init {
-        super.setValue(getFromSp(sp, key, default))
+        super.setValue(sp.get())
+        super.setValue(sp.get())
         sp.registerOnSharedPreferenceChangeListener(this)
     }
     
-    abstract fun getFromSp(sp: SharedPreferences, key: String, default: T): T
-    
-    abstract fun putToSp(editor: SharedPreferences.Editor, key: String, value: T)
     
     override fun onSharedPreferenceChanged(sp: SharedPreferences, key: String) {
         if (key == this.key) {
-            super.setValue(getFromSp(sp, key, default))
+            super.setValue(get(sp))
         }
     }
     
@@ -65,7 +59,7 @@ abstract class SharedPreferenceLiveData<T>(
     
     public override fun setValue(value: T) {
         val editor = sp.edit()
-        putToSp(editor, key, value)
+        editor.put(value)
         editor.apply()
     }
 }
